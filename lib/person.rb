0aee424e9e11bc9bbaeb751e30123638e47b8f0a
samanthamarberger <slatham04@gmail.com>
2023-03-10 13:02:16 -0700

    done

--- lib/person.rb
+++ lib/person.rb
@@ -31,13 +31,47 @@ class Person
         end
     end
 
-
-# STUCK HERe
     def happy?
-        happiness >= 7?
+        happiness > 7
     end
 
     def clean?
-        hygiene >= 7?
+        hygiene > 7
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            return "blah blah sun blah rain"
+        else
+            return "blah blah blah blah blah"
+        end
     end
 end
